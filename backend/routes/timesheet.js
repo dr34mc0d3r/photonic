b5f4ebfb13dccad465065df14c51770391e283c3
Chris Jackson <root.operator@gmail.com>
2020-07-31 10:14:56 -0400

    defining API details

--- backend/routes/timesheet.js
+++ backend/routes/timesheet.js
@@ -1,9 +1,24 @@
 const router = require('express').Router();
 const verifyToken = require('./verifyToken');
 
+
+// get list of all timesheet entries
 router.get('/', verifyToken, (req, res) => {
     res.json({posts:{title: "sadasddadaddas", description: "dfsdfsdf"}});
 });
 
+// get timesheet entry for date
+// '/timesheet:date'
+
+// get a timesheet by ID - this includes all associated timesheet items
+// '/timesheet:id'
+
+// PUT (update) timesheet for ID
+// '/timesheet:id'
+
+// delete timesheet by ID - and all associated timesheet items
+// '/delete:id'
+
+// new timesheet
 
 module.exports = router;
