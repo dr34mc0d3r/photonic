e9a7725bf0485a1e527ad29301407f8625bef7d4
Chris Jackson <root.operator@gmail.com>
2020-08-04 17:53:02 -0400

    include user_id in timesheet schema

--- backend/model/TimesheetItem.js
+++ backend/model/TimesheetItem.js
@@ -1,6 +1,11 @@
 const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
+    user_id: {
+        type: String,
+        required: true,
+        min: 3
+    },
     start_time: {
         type: String,
         required: true,
